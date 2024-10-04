8795b2338868fbee64db18e48ccd070fa2894132
Bogdan <Icestorm23@mail.ru>
2024-10-04 21:57:54 +0700

    Task1_update

--- task1_1.py
+++ task1_1.py
@@ -1,11 +1,12 @@
 #Задание №1
-
 def multiplication_table(n):
+    print(" " * 4, end="")
     for i in range(1, n + 1):
         print(f"{i:4}", end="")
     print()
 
     for i in range(1, n + 1):
+        print(f"{i:4}", end="")
         for j in range(1, n + 1):
             print(f"{i * j:4}", end="")
         print()
