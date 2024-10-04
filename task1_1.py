873737f8632fe599ce73cc1f9e03722248336983
Bogdan <Icestorm23@mail.ru>
2024-10-04 20:50:31 +0700

    Task1

---
+++ task1_1.py
@@ -0,0 +1,17 @@
+#Задание №1
+
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        print(f"{i:4}", end="")
+    print()
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print(f"{i * j:4}", end="")
+        print()
+
+#если нужно ввести число 1-10
+#s1=int(input())
+
+#пример из задания с 5
+multiplication_table(5)
